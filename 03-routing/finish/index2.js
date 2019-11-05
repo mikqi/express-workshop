95769e4053e4a0b01f7964fcfcb1246375cd0ca5
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-11-06 01:05:26 +0700

    add(lessons-3): add multer handle formdata

--- 03-routing/finish/index2.js
+++ 03-routing/finish/index2.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const multer = require('multer')
+
+const upload = multer()
 
 const routeAPI = require('./api')
 
 const app = express()
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
 
 app.use('/api', routeAPI)
+
+// Can handle req.body form-data
+app.post('/upload', upload.any(), (req, res) => {
+  res.json({
+    image: req.files[0]
+  })
+})
 
 app.listen(3001, () => {
   console.log('app run on http://localhost:3001')
